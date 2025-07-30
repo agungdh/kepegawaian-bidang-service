9659db4e95bfea875e6ad55190a6bb581b90ad23
AgungDH <agungdh@live.com>
2025-07-30 17:30:07 +0700

    init

---
+++ src/main/java/id/my/agungdh/kepegawaianbidangservice/KepegawaianBidangServiceApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.kepegawaianbidangservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KepegawaianBidangServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KepegawaianBidangServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/kepegawaianbidangservice/KepegawaianBidangServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.kepegawaianbidangservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KepegawaianBidangServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
